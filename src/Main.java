c006464dda9052a255c1fb7d4b767e6fe55562fa
tassiosantos <tassiosantos@gmail.com>
2023-06-27 21:34:57 -0300

    End exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Solução 1
+//        Scanner read = new Scanner(System.in);
+//        int num;
+//
+//        System.out.println("Digite um valor inteiro:");
+//
+//        num = read.nextInt();
+//
+//        if(num > 0){
+//            System.out.println("Número positivo");
+//        } else if (num == 0) {
+//            System.out.println("Número zero");
+//        }else{
+//            System.out.println("Número negativo");
+//        }
+
+        //Solução 2
+//        Scanner read = new Scanner(System.in);
+//        int num;
+//        System.out.println("Digite um valor inteiro:");
+//        num = read.nextInt();
+//
+//        if(num%2 == 0){
+//            for(int i = 0; i<=num/2; i++){
+//                System.out.println(2*i);
+//            }
+//        }else{
+//            for(int i = 0; i<=(num-1)/2; i++){
+//                System.out.println(2*i);
+//            }
+//        }
+
+        //Solução 3
+//        Scanner read = new Scanner(System.in);
+//        int num;
+//        System.out.println("Digite um valor inteiro positivo:");
+//        num = read.nextInt();
+//        int i = 1;
+//
+//        while(i <= num){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //Solução 4
+//        Scanner read = new Scanner(System.in);
+//        int num;
+//        int numeroSorte = 5;
+//
+//        do {
+//            System.out.println("Digite o número da sorte entre 1 e 10:");
+//            num = read.nextInt();
+//
+//        }while(num != numeroSorte);
+//        if(num == numeroSorte){
+//            System.out.println("Acertou o número: " + numeroSorte);
+//        }
 
     }
 }
